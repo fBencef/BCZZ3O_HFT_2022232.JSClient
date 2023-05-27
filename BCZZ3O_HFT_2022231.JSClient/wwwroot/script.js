cb67b4cbc0c3ec2b8de52a28c25e7abbf314222a
Bence Farkas <bence.farkas01@gmail.com>
2023-05-27 20:44:29 +0200

    Create and Delete

--- BCZZ3O_HFT_2022231.JSClient/wwwroot/script.js
+++ BCZZ3O_HFT_2022231.JSClient/wwwroot/script.js
@@ -1,3 +1,67 @@
-﻿fetch('http://localhost:47322/vehicle')
-    .then(x => x.json())
-    .then(y => console.log(y));
+﻿let vehicles = [];
+getData();
+
+async function getData() {
+    await fetch('http://localhost:47322/vehicle')
+        .then(x => x.json())
+        .then(y => {
+            vehicles = y;
+            console.log(vehicles);
+            display();
+        });
+}
+
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    vehicles.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.displayReg + "</td><td>"
+            + t.manufacturer + "</td><td>"
+            + t.model + "</td><td>"
+            + t.length + "</td><td>"
+            + `<button type="button" onclick="remove(${t.registration})">Delete</button>`
+            + "</td></tr>";
+        //console.log(t.registration)
+    });
+}
+function create() {
+    let registration = document.getElementById('vehiclereg').value;
+    let displayReg = registration;
+    let manufacturer = document.getElementById('vehiclemanufact').value;
+    let model = document.getElementById('vehiclemodel').value;
+    let length = document.getElementById('vehiclelength').value;
+
+    fetch('http://localhost:47322/vehicle', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            {
+                registration: registration,
+                displayReg: displayReg,
+                manufacturer: manufacturer,
+                model: model,
+                length: length,
+                registrationDate: Date.now
+            }),})
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
+
+function remove(registration) {
+    fetch('http://localhost:47322/vehicle/' + registration, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
