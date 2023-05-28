37a2e0c53274c7c5c5a0ff20afe77fc59e9e1aeb
Bence Farkas <bence.farkas01@gmail.com>
2023-05-28 11:34:52 +0200

    Vehicle UI

--- BCZZ3O_HFT_2022231.JSClient/wwwroot/script.js
+++ BCZZ3O_HFT_2022231.JSClient/wwwroot/script.js
@@ -1,12 +1,51 @@
 ﻿let vehicles = [];
+let connection = null;
+
+let vehicleIdToUpdate = '';
+
 getData();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:47322/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("VehicleCreated", (user, message) => {
+        getData();
+    });
+
+    connection.on("VehicleDeleted", (user, message) => {
+        getData();
+    });
+
+    connection.on("VehicleUpdated", (user, message) => {
+        getData();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
 
 async function getData() {
     await fetch('http://localhost:47322/vehicle')
         .then(x => x.json())
         .then(y => {
             vehicles = y;
-            console.log(vehicles);
+            //console.log(vehicles);
             display();
         });
 }
@@ -21,9 +60,23 @@ function display() {
             + t.model + "</td><td>"
             + t.length + "</td><td>"
             + `<button type="button" onclick="remove(${t.registration})">Delete</button>`
+            + `<button type="button" onclick="showupdate(${t.registration})">Update</button>`
             + "</td></tr>";
         //console.log(t.registration)
     });
+}
+
+function showupdate(registration) {
+    document.getElementById('updateformdiv').style.display = 'flex';
+
+    document.getElementById('vehicleregtoupdate').value = vehicles.find(t => t['registration'] == registration)['registration']
+    document.getElementById('displayregtoupdate').value = vehicles.find(t => t['registration'] == registration)['displayReg']
+    document.getElementById('vehiclemanufacttoupdate').value = vehicles.find(t => t['registration'] == registration)['manufacturer']
+    document.getElementById('vehiclemodeltoupdate').value = vehicles.find(t => t['registration'] == registration)['model']
+    document.getElementById('vehiclelengthtoupdate').value = vehicles.find(t => t['registration'] == registration)['length']
+
+
+    vehicleIdToUpdate = registration;
 }
 function create() {
     let registration = document.getElementById('vehiclereg').value;
@@ -43,7 +96,37 @@ function create() {
                 model: model,
                 length: length,
                 registrationDate: Date.now
-            }),})
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
+
+function update() {
+    document.getElementById('updateformdiv').style.display = 'none';
+
+    //let displayReg = document.getElementById('displayregtoupdate').value;
+    let manufacturer = document.getElementById('vehiclemanufacttoupdate').value;
+    let model = document.getElementById('vehiclemodeltoupdate').value;
+    let length = document.getElementById('vehiclelengthtoupdate').value;
+
+    fetch('http://localhost:47322/vehicle', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            {
+                registration: vehicleIdToUpdate,
+                displayReg: displayReg,
+                manufacturer: manufacturer,
+                model: model,
+                length: length,
+                registrationDate: registrationDate
+            }),
+    })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
@@ -53,6 +136,7 @@ function create() {
 }
 
 function remove(registration) {
+
     fetch('http://localhost:47322/vehicle/' + registration, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json', },
@@ -61,7 +145,7 @@ function remove(registration) {
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
-            getdata();
+            getData();
         })
         .catch((error) => { console.error('Error:', error); });
 }
